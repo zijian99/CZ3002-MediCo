ceaa91d981cc63040ada3d145d8d6e2de20f84dd
MINK <76023265+mink0003@users.noreply.github.com>
2022-08-17 16:58:14 +0800

    Add placeholder landing page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Typography } from "@mui/material";
+
+export default function Home() {
+	return <Typography variant='h1'>This is the landing page</Typography>;
+}
