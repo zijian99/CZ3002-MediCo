141d8557b858995d210e2ce171dfe1ec097d39fb
Anthony Zou <zouzeren@gmail.com>
2022-08-24 14:31:07 +0800

    Installed drawing Canvas npm
    Added Login, Register, SystemDeclaration

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import SymptomDelcation from "./pages/SymptomDeclaration";
 function App() {
 	return (
 		<div className='App'>
 			<BrowserRouter>
 				<Routes>
 					<Route path='/' element={<Home />} />
+					<Route path='/Login' element={<Login />} />
+					<Route path='/Register' element={<Register />} />
+					<Route path='/SymptomDelcation' element={<SymptomDelcation />} />
 				</Routes>
 			</BrowserRouter>
 		</div>
---
+++ src/pages/SymptomDeclaration.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { Typography } from "@mui/material";
+
+export default function SymptomDelcation() {
+	return <Typography variant='h1'>This is the Symptom Delcation page</Typography>;
+}
