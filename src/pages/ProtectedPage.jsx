18d347d5e15f3cc599588c47dc0c368194576b7c
MINK <76023265+mink0003@users.noreply.github.com>
2022-08-26 15:52:41 +0800

    Add ProtectedPage

---
+++ src/pages/ProtectedPage.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect, } from "react";
+import { auth } from "../firebase";
+
+export default function ProtectedPage(props){
+    useEffect(() => {
+        //Set up observer on user authentication 
+        onAuthStateChanged(auth, (user) => {
+        if (user) {
+            // User is signed in
+            console.log("Authorization granted.");
+            props.setLoggedIn(true);
+        } else {
+            // User is signed out
+            console.log("Not authorized.");
+            props.setLoggedIn(false);
+        }
+        });    
+    });
+    return props.loggedIn ? (<div><h1>Protected content</h1></div>) : (<div><h1>Not logged in</h1></div>);
+}
