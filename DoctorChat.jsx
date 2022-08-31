3a36601e28b3d2a8b0f07d17d4584bf219413579
MINK <76023265+mink0003@users.noreply.github.com>
2022-08-31 17:28:10 +0800

    Add DoctorChat, ChatScreen and ChatBar files

---
+++ DoctorChat.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+import { auth } from "../firebase";
+import { onAuthStateChanged } from "firebase/auth";
+import { Grid, CircularProgress } from "@mui/material";
+
+export default function DoctorChat(props) {
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        //Set up observer on user authentication
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                // User is signed in
+                console.log("Authorization granted.");
+                props.setLoggedIn(true);
+                setLoading(false);
+            } else {
+                // User is signed out
+                console.log("Not authorized.");
+                props.setLoggedIn(false);
+                setLoading(false);
+            }
+        });
+    });
+    return loading ? (
+        <Grid
+            container
+            spacing={0}
+            direction="column"
+            alignItems="center"
+            justifyContent="center"
+            sx={{ minHeight: "90vh" }}
+        >
+            <CircularProgress size={60} />
+        </Grid>
+    ) : props.loggedIn ? (
+        <div>
+            <h1>Protected content</h1>
+        </div>
+    ) : (
+        <div>
+            <h1>Not logged in</h1>
+        </div>
+    );
+}
+
+
+
---
+++ src/components/ChatBar.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import { TextField, Button } from "mui/material"
+
+export default function ChatBar(props){
+
+	return (
+		<Grid container>
+			<Grid item>
+				<TextField multiline onChange={} onKeyDown={} variant='filled'/>
+			</Grid>
+			<Grid item>
+
+			</Grid>
+		</Grid>
+	);
+}
