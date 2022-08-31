7b1792588778d4720398e2f72550d52415a8b1cd
MINK <76023265+mink0003@users.noreply.github.com>
2022-08-31 17:39:12 +0800

    Update DoctorChat page

--- src/pages/DoctorChat.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { Typography } from "@mui/material";
-
-export default function DoctorChat() {
-	return <Typography variant='h1'>This is the  Chat page</Typography>;
-}
