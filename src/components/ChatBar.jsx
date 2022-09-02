566908b36daa10c232bc3dc98dea81492d4b3198
MINK <76023265+mink0003@users.noreply.github.com>
2022-09-02 16:00:52 +0800

    Add handlers for ChatBar

--- src/components/ChatBar.jsx
+++ src/components/ChatBar.jsx
@@ -2,14 +2,35 @@ import React from "react"
 import { TextField, Button } from "mui/material"
 
 export default function ChatBar(props){
+	const [currentText, setCurrentText] = useState("");
+
+	const handleChange = (event) => {
+		setCurrentText((currentText) => event.target.value);
+	}
+
+	const handleKeyDown = (event) =>{
+		if (event.key == "Enter" && event.shiftKey) {
+		  return;
+		} else if (event.key == "Enter") {
+		  handleSend();
+		  event.preventDefault();
+		}	
+	}
+
+	const handleSend = () =>{
+		if(currentText === ""){ // Empty message
+			return;
+		}
+		// Send to database:
+	}
 
 	return (
 		<Grid container>
 			<Grid item>
-				<TextField multiline onChange={} onKeyDown={} variant='filled'/>
+				<TextField multiline value={currentText} onChange={handleChange} onKeyDown={handleKeyDown} variant='filled'/>
 			</Grid>
 			<Grid item>
-
+				<Button variant='filled'>SEND</Button>
 			</Grid>
 		</Grid>
 	);
