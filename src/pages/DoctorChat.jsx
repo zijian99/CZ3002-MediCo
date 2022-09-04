a341ee0e2867ea60b4f37aae2efa81c1dc6dae5a
MINK <76023265+mink0003@users.noreply.github.com>
2022-09-04 09:51:58 +0800

    Adjust Grid height for ChatWindow in DoctorChat

--- src/pages/DoctorChat.jsx
+++ src/pages/DoctorChat.jsx
@@ -1,54 +1,53 @@
-import React, { useEffect, useState } from "react";
-import { auth } from "../firebase";
-import { onAuthStateChanged } from "firebase/auth";
-import { Grid, CircularProgress, Button } from "@mui/material";
-import ChatBar from "../components/ChatBar.jsx";
-import ChatScreen from "../components/ChatScreen.jsx";
-
+import React, { useEffect, useState } from 'react';
+import { auth } from '../firebase';
+import { onAuthStateChanged } from 'firebase/auth';
+import { Grid, CircularProgress, Button } from '@mui/material';
+import ChatBar from '../components/ChatBar.jsx';
+import ChatWindow from '../components/ChatWindow';
 export default function DoctorChat(props) {
-  const [loading, setLoading] = useState(true);
-  const [userName, setUserName] = useState(null);
-  useEffect(() => {
-    //Set up observer on user authentication
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        // User is signed in
-        console.log("Authorization granted.");
-        props.setLoggedIn(true);
-        // Call users collection to get username
-        //setUserName(...)
-        setLoading(false);
-      } else {
-        // User is signed out
-        console.log("Not authorized.");
-        props.setLoggedIn(false);
-        setLoading(false);
-      }
+    const [loading, setLoading] = useState(true);
+    const [userName, setUserName] = useState(null);
+    useEffect(() => {
+        //Set up observer on user authentication
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                // User is signed in
+                console.log('Authorization granted.');
+                props.setLoggedIn(true);
+                // Call users collection to get username
+                //setUserName(...)
+                setLoading(false);
+            } else {
+                // User is signed out
+                console.log('Not authorized.');
+                props.setLoggedIn(false);
+                setLoading(false);
+            }
+        });
     });
-  });
-  return loading ? (
-    <Grid
-      container
-      spacing={0}
-      direction="column"
-      alignItems="center"
-      justifyContent="center"
-      sx={{ minHeight: "90vh" }}
-    >
-      <CircularProgress size={60} />
-    </Grid>
-  ) : props.loggedIn ? (
-    <Grid container direction="column" justifyContent="flex-start">
-      <Grid item xs={1} sx={{ minHeight: "80vh" }}>
-        <ChatScreen userName={userName} />
-      </Grid>
-      <Grid item xs={6} alignItems="flex-end" justifyContent="flex-end">
-        <ChatBar />
-      </Grid>
-    </Grid>
-  ) : (
-    <div>
-      <h1>Not logged in</h1>
-    </div>
-  );
+    return loading ? (
+        <Grid
+            container
+            spacing={0}
+            direction='column'
+            alignItems='center'
+            justifyContent='center'
+            sx={{ minHeight: '90vh' }}
+        >
+            <CircularProgress size={60} />
+        </Grid>
+    ) : props.loggedIn ? (
+        <Grid container direction='column' justifyContent='flex-start'>
+            <Grid item xs={1} sx={{ minHeight: '80vh' }}>
+                <ChatWindow />
+            </Grid>
+            <Grid item xs={6} alignItems='flex-end' justifyContent='flex-end'>
+                <ChatBar />
+            </Grid>
+        </Grid>
+    ) : (
+        <div>
+            <h1>Not logged in</h1>
+        </div>
+    );
 }
